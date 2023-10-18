0b7468cd7255a53514db1eedd1016e17786e03af
babitaism <rbabita044@gmail.com>
2023-10-18 15:33:23 +0530

    routing updated in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "../src/style/App.css";
 import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../src/components/Login"
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Login(){
+return(
+    <div>Hi</div>
+)
+
+}
+
+export default Login
